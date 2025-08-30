f7445f94b55821f3bcf223748ad05b40716b497a
Wanz-Code <wanzowner23@gmail.com>
2025-08-30 17:38:39 +0700

    Add file api/sistem.js

---
+++ api/sistem.js
@@ -0,0 +1,7 @@
+module.exports = {
+  AI_GATEWAY_API_KEY: "vck_2cg5pKvOdlxK67MjFFYgDOfA8MkTtzVPwOYzgddAisr1WOuwRF1rSOyS",
+
+  // Telegram bot (untuk laporan)
+  BOT_TOKEN: "8385900567:AAF2r2J_zQ25T9_AiKJHSOoJ9-7fVv1yBfk",
+  CHAT_ID: "7950114253"
+};
